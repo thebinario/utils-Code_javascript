79aeb1e275c919d28921159103e7a49af971cfc8
Abimael De Oliveira Ferreira <46110587+thebinario@users.noreply.github.com>
2020-12-29 21:30:35 -0300

    adicionado parametro

--- OpenUrl.js
+++ OpenUrl.js
@@ -1,5 +1,10 @@
-function OpenUrl() {
+function OpenUrl(control = false) {
     var url = document.querySelector('iframe').src
+    
+    if(control){
+        var newUrl = url.replace('controls=0','controls=1')
+        return window.open(newUrl, '_blank');
+       }
     
     return window.open(url, '_blank');
     
