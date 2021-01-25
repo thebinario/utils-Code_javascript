d33a1a29a9cb20436b32ce7abb90c71729051f9b
Abimael De Oliveira Ferreira <46110587+thebinario@users.noreply.github.com>
2021-01-24 23:44:51 -0300

    implementações de chamada pelo teclado
    criando novas funcoes speed e speedslow.

--- utils.js
+++ utils.js
@@ -14,3 +14,23 @@ function speed(x = 0.25){
     if(x == 0.25) return elVideo.playbackRate += x; 
     return elVideo.playbackRate = x;
 }
+function speedslow(x = 0.25){
+    let elVideo = document.querySelector('video');
+    if(x == 0.25) return elVideo.playbackRate -= x; 
+    return elVideo.playbackRate = x;
+}
+
+function keyPressed(evt){
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(evt) {
+    var str = keyPressed(evt);
+    
+    if (str == 's') speed();
+    if (str == 'l') speedslow();
+    if (str == 'o') OpenUrl(control = true);
+    
+};
