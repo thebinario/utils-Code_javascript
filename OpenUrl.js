4481527d8b935016d2a48ece771e466d2a111e41
Abimael De Oliveira Ferreira <46110587+thebinario@users.noreply.github.com>
2021-01-24 23:19:05 -0300

    funcao speed imprementada

--- OpenUrl.js
+++ OpenUrl.js
@@ -1,11 +1,16 @@
+//Abre url do video em uma nova aba
 function OpenUrl(control = false) {
     var url = document.querySelector('iframe').src
     
     if(control){
         var newUrl = url.replace('controls=0','controls=1')
         return window.open(newUrl, '_blank');
-       }
-    
-    return window.open(url, '_blank');
-    
+       }    
+    return window.open(url, '_blank');    
+}
+//Acelera o video
+function speed(x = 0.25){
+    let elVideo = document.querySelector('video');
+    if(x == 0.25) return elVideo.playbackRate += x; 
+    return elVideo.playbackRate = x;
 }
